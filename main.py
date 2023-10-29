0df018d4b481ce6d2888f99eacb33f39e6e1b9c1
Mir <kalonov.01.m@gmail.com>
2023-10-30 00:28:39 +0700

    changed source link

--- main.py
+++ main.py
@@ -7,3 +7,6 @@ crawler.crawl([
     'https://habr.com/ru/articles/756800/',
     'https://www.nstu.ru/studies'
                 ])
+
+get_top_20_domain = crawler.get_top_20_domain()
+print(get_top_20_domain)
